06dadd8ad0e3f102041216215711c3572160e36a
Anton Yulbarysau <io57196657@gmail.com>
2021-09-20 13:40:48 +0300

    add: check notes

--- src/store/sagas/notes.ts
+++ src/store/sagas/notes.ts
@@ -5,42 +5,21 @@ import {
     takeEvery, 
 } from 'redux-saga/effects';
 import { Note } from '../../interfaces';
+import { sortNotes } from '../../utils';
 import actions from '../actions';
 
 type Params = { id: string, note: Note, notes: Note[], filter: string, type: string };
 
-function sortNotes(notes: Note[], note: any, filter: string) {
-    if (typeof note[filter] === 'string') {
-        return sortByText(notes, filter);
-    } else {
-        return sortByNumber(notes, filter);
-    }
-};
-
-function sortByText(notes: Note[], filter: string) {
-    return notes.sort((prev: any, next: any) => {
-        return next[filter].localeCompare(prev[filter]);
-    });
-};
-
-function sortByNumber(notes: Note[], filter: string) {
-    return notes.sort((prev: any, next: any) => {
-        return prev[filter] - next[filter];
-    });
-};
-
 function* fetchNotes({ filter }: Params) {
     yield delay(1500);
 
-    const notes = (JSON.parse(localStorage.getItem('notes')!) || []);
+    let notes = JSON.parse(localStorage.getItem('notes')!) || [];
 
-    console.log(notes)
+    if (notes) {
+        notes = sortNotes(notes, notes[0], filter);
+    }
 
-    const sortedNotes = sortNotes(notes, notes[0], filter);
-    
-    console.log(sortedNotes)
-
-    yield put({ type: actions.FETCH_NOTES_SUCCESS, payload: sortedNotes });
+    yield put({ type: actions.FETCH_NOTES_SUCCESS, payload: notes });
 };
 
 function* deleteNotes({ notes }: Params) {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,19 +1,21 @@
-// function sortNotes(notes: Note[], note: any, filter: string) {
-//     if (typeof note[filter] === 'string') {
-//         return sortByText(notes, filter);
-//     } else {
-//         return sortByNumber(notes, filter);
-//     }
-// };
+import { Note } from '../interfaces';
 
-// function sortByText(notes: Note[], filter: string) {
-//     return notes.sort((prev: any, next: any) => {
-//         return next[filter].localeCompare(prev[filter])
-//     });
-// };
+export function sortNotes(notes: Note[], note: any, filter: string) {
+    if (typeof note[filter] === 'string') {
+        return sortByText(notes, filter);
+    } else {
+        return sortByNumber(notes, filter);
+    }
+};
 
-// function sortByNumber(notes: Note[], filter: string) {
-//     return notes.sort((prev: any, next: any) => {
-//         return prev[filter] - next[filter];
-//     });
-// };
+export function sortByText(notes: Note[], filter: string) {
+    return notes.sort((prev: any, next: any) => {
+        return next[filter].localeCompare(prev[filter])
+    });
+};
+
+export function sortByNumber(notes: Note[], filter: string) {
+    return notes.sort((prev: any, next: any) => {
+        return prev[filter] - next[filter];
+    });
+};
