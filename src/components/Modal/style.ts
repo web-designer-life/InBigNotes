451f2fde8979cc6bb78481f018dbf7edda81e687
Anton Yulbarysau <io57196657@gmail.com>
2021-09-22 12:40:55 +0300

    refact: adaptive

--- src/components/Modal/style.ts
+++ src/components/Modal/style.ts
@@ -21,7 +21,7 @@ const ModalContainer = styled.div`
     justify-content: center;
     align-items: center;
     border-radius: 20px;
-    width: 400px;
+    min-width: 400px;
     padding: 25px;
     margin: 25px;
     background-color: #FFFFFF;
