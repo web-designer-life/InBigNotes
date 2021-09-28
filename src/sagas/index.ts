2c1ce2eb4c3b60aa7c8e49afe067c2cb6a852a92
Anton Yulbarysau <io57196657@gmail.com>
2021-09-28 09:59:11 +0300

    refact: name of saga

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,12 +1,12 @@
 import { all } from 'redux-saga/effects';
-import navigate from './navigate';
 import noteSaga from './note';
 import notesSaga from './notes';
+import navigateSaga from './navigate';
 
 export default function* rootSaga() {
     yield all([
         noteSaga(),
         notesSaga(),
-        navigate(),
+        navigateSaga(),
     ]);
 };
